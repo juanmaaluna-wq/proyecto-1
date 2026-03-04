28a28f6e3e4a7960a6393e73d27596f09c437d45
manuel <juanma.aluna@gmail.com>
2026-03-04 07:48:18 -0500

    cambios

---
+++ src/assets/componentes/MatematicasMagicas.jsx
@@ -0,0 +1,16 @@
+
+function MatematicasMagicas() {
+  const nombre = "Alex";
+  const edad = 17;
+  const anioNacimiento = 2024 - edad; // Calculamos algo rápido
+
+  return (
+    <div className="tarjeta">
+      <h2>Perfil de {nombre}</h2>
+      <p>Si tienes {edad} años, naciste aproximadamente en {anioNacimiento}.</p>
+      <p>¿Cuánto es 5 por 5? ¡Es {5 * 5}!</p>
+    </div>
+  );
+}
+
+export default MatematicasMagicas;
---
+++ src/assets/componentes/Saludo.jsx
@@ -0,0 +1,11 @@
+
+function Saludo() {
+  return (
+    <div>
+      <h1>¡Hola, Universo!</h1>
+      <p>Este es mi primer bloque de LEGO en React.</p>
+    </div>
+  );
+}
+
+export default Saludo;
